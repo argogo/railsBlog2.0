8bed9601ab99601264818a8ddc0c747a91d490c7
arpcso <a.gomez18384@gmail.com>
2017-03-04 20:23:49 -0800

    1.0.0 Create manual blog scaffold

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'posts#index', as: 'home'  
+  #roots landing page to posts/index 
+
+  get 'about' => 'pages#about', as: 'about'
+  #gets pages/about.html.erb
+
+  resources :posts
+  #gives posts resources
+
+end
